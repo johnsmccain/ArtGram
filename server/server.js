37de9da185b7153b18648aee58ddd4774ce84a2b
holabayor <liasu.olabayo@gmail.com>
2023-03-13 22:06:40 +0100

    server config

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import morgan from 'morgan';
-import appRouter from './routes/index';
+import router from './routes/index';
 import dbClient from './utils/db';
+
 const port = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.use(morgan('dev'));
 app.use(express.json());
-app.use('/', appRouter);
+app.use('/', router);
+
+app.disable('x-powered-by');
 
 app.listen(port, () => {
   dbClient();
