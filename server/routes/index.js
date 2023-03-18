5aea39c0451a28374a63b5f82cf7a6c6b284141d
holabayor <liasu.olabayo@gmail.com>
2023-03-18 22:29:46 +0100

    index

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,6 +9,10 @@ import express from 'express';
 
 const router = express.Router();
 
+router.get('/', sayHello);
+router.get('/signup', register);
+router.post('/login', AuthController.logIn);
+
 router.post('/signup', UsersController.createUser);
 
 router.get('/login', AuthController.logIn);
