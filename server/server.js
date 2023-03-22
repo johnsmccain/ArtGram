f5a72262af17458ddfb0148d567674dca99f25e0
holabayor <liasu.olabayo@gmail.com>
2023-03-22 21:22:07 +0100

    update

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import morgan from 'morgan';
 import router from './routes/index';
 import dbClient from './utils/db';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -9,7 +13,14 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.use(morgan('dev'));
+
+// Set up cors to allow requests from client
+app.use(cors());
+
+app.use(express.json());
 app.use('/', router);
 
 app.disable('x-powered-by');
