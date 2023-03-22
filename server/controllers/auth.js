3e5279c9e121fcdb540e4a24f8fcd33aa44940c6
holabayor <liasu.olabayo@gmail.com>
2023-03-22 21:39:35 +0100

    server update

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,46 +1,73 @@
-import crypto from 'crypto';
-import { v4 as uuidv4 } from 'uuid';
-import redisClient from '../utils/redis';
+import bcrypt from 'bcrypt';
 import User from '../models/user';
-import { Types } from 'mongoose';
+import jwt from 'jsonwebtoken';
 
 class AuthController {
   static async logIn(req, res) {
     try {
-      const authToken = req.header('Authorization').split(' ')[1];
-      const auth = Buffer.from(authToken, 'base64').toString('utf8');
-      const [email, password] = auth.split(':');
-      const hash = crypto.createHash('sha256').update(password).digest('hex');
-      const user = await User.findOne({ email: email });
-      // console.log(!user);
-      // if (!user) {
-      //   res.status(403).send({ error: 'User not found' });
-      //   return;
-      // }
-      const key = `auth_${uuidv4()}`;
-      redisClient.set(key, user._id.toString(), 86400);
-      res.status(200).send({ message: 'Logged in successfully' });
-    } catch (e) {
-      res.status(403).send({ error: 'User not found' });
+      if (!req.body.email || !req.body.password) {
+        res
+          .status(403)
+          .json({ error: 'Please, enter your email and password' });
+        return;
+      }
+      User.findOne({ email: req.body.email }).then((user) => {
+        if (!user) {
+          res.status(403).json({ error: 'Invalid email' });
+          return;
+        }
+        bcrypt.compare(req.body.password, user.password).then((isMatch) => {
+          if (isMatch) {
+            const accessToken = jwt.sign(
+              { id: user._id },
+              process.env.ACCESS_TOKEN_SECRET,
+              {
+                expiresIn: 1200,
+              }
+            );
+            const refreshToken = jwt.sign(
+              { id: user._id },
+              process.env.REFRESH_TOKEN_SECRET,
+              {
+                expiresIn: '7d',
+              }
+            );
+            res.cookie('refreshToken', refreshToken, { httpOnly: true });
+            res.setHeader('Authorization', `Bearer ${accessToken}`);
+            res.status(200).send({
+              accessToken,
+              refreshToken,
+              user,
+            });
+          } else {
+            res.status(403).json({ error: 'Invalid password' });
+          }
+        });
+      });
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(403);
     }
   }
+
   static async logOut(req, res) {
-    const token = req.header('X-Token');
-    const key = `auth_${token}`;
-
-    const userId = await redisClient.get(key);
-
+    const authHeader = req.headers['authorization'];
+    console.log(authHeader);
+    const userId = req.userId;
+    console.log('The userId is ' + userId);
     if (!userId) {
       res.status(401).send({ error: 'Unauthorized' });
       return;
     }
-    const user = User.findOne({ _id: new Types.ObjectId(userId) });
-    if (!user) {
-      res.status(401).send({ error: 'Unauthorized' });
-      return;
-    }
-    await redisClient.del(key);
-    res.status(204).send({});
+
+    jwt.sign(authHeader, '', { expiresIn: 1 }, (error) => {
+      if (error) {
+        res.status(401).send({ error: 'not signed' });
+        return;
+      }
+      res.status(204).send({ message: 'Logged Out' });
+      res.clearCookies('refreshToken');
+    });
   }
 
   static async resetPassword(req, res) {}
