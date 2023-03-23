06cef5f7b50e962d0379edc67d99bfb24bed9283
johnsmccain <johnsdanlami@gmail.com>
2023-03-23 12:08:16 +0100

    login & signup UI

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import Vid from "../assets/share.mp4";
+
+const inputWrapper = `my-4  bg-slate-300 flex rounded-md items-center `;
+const inputLabel = `ml-3`;
+const inputControll = `p-2 border-none outline-none bg-transparent`;
+
 const Login = () => {
 	const navigate = useNavigate();
 	return (
@@ -16,10 +21,33 @@ const Login = () => {
 					src={Vid}
 				/>
 				<div className="absolute flex justify-center items-center flex-col bg-blackOverlay top-0 left-0 right-0 bottom-0">
-					LOGIN
+					<form className="bg-slate-100 rounded-lg p-5 text-black flex justify-center align-middle">
+						<div className="w-full flex flex-col justify-center">
+							<div className={inputWrapper}>
+								<label htmlFor="email" className={inputLabel}>
+									Email:
+								</label>
+								<input type="email" className={inputControll} id="email" />
+							</div>
+							<div className={inputWrapper}>
+								<label htmlFor="password" className={inputLabel}>
+									Password:
+								</label>
+								<input
+									type="password"
+									className={inputControll}
+									id="password"
+								/>
+							</div>
+							<button
+								type="submit"
+								className="bg-red-600 text-white font-semibold rounded-lg p-2 hover:bg-red-500 active:bg-red-400 mt-1">
+								SignUp
+							</button>
+						</div>
+					</form>
 				</div>
 			</div>
-			<button onClick={() => navigate("/signup")}>SignUp here!</button>
 		</div>
 	);
 };
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Vid from "../assets/share.mp4";
 
+const inputWrapper = `my-4  bg-slate-300 flex  rounded-md items-center `;
+const inputLabel = `ml-3`;
+const inputControll = `p-2 border-none outline-none bg-transparent`;
 const Register = () => {
 	return (
 		<div className="flex h-screen items-center justify-start flex-col">
@@ -15,7 +18,37 @@ const Register = () => {
 					src={Vid}
 				/>
 				<div className="absolute flex justify-center items-center flex-col bg-blackOverlay top-0 left-0 right-0 bottom-0">
-					hhhh
+					<form className="bg-slate-100 rounded-lg p-5 text-black flex justify-center align-middle">
+						<div className="w-full flex flex-col justify-center">
+							<div className={inputWrapper}>
+								<label htmlFor="name" className={inputLabel}>
+									Name:
+								</label>
+								<input type="text" className={inputControll} id="name" />
+							</div>
+							<div className={inputWrapper}>
+								<label htmlFor="email" className={inputLabel}>
+									Email:
+								</label>
+								<input type="email" className={inputControll} id="email" />
+							</div>
+							<div className={inputWrapper}>
+								<label htmlFor="name" className={inputLabel}>
+									Password:
+								</label>
+								<input
+									type="password"
+									className={inputControll}
+									id="password"
+								/>
+							</div>
+							<button
+								type="submit"
+								className="bg-red-600 text-white font-semibold rounded-lg p-2 hover:bg-red-500 active:bg-red-400 mt-1">
+								SignUp
+							</button>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
